647c6d90d1b18e3c335754ef5d96401e6521f415
Florian Eggenhofer <egg@tbi.univie.ac.at>
2019-01-18 01:09:18 +0100

    Updated configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,5 @@
+export CC="x86_64-conda_cos6-linux-gnu-cc"
+export LD="x86_64-conda_cos6-linux-gnu-ld"
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
