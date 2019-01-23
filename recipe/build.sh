1bd34962b5a8660c0494b5f38a42352d5ae5f932
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-24 00:10:44 +0100

    Fixed newly added lib path

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
-perl -pi -e 's/("C compiler link flags", " "),/("C compiler link flags", " -L$PREFIX\/lib"),/g' settings
+perl -pi -e 's/\("C compiler link flags", " "\),/("C compiler link flags", " -L$PREFIX\/lib -lgmp "),/g' settings
 make install
 cat $PREFIX/lib/ghc-8.2.2/settings
