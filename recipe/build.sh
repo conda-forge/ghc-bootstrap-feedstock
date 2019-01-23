9881930877a9393cacb6b936fc27bd05ed1144c4
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-23 23:56:02 +0100

    Fixed build number

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,5 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 perl -pi -e 's/("C compiler link flags", " "),/("C compiler link flags", " -L$PREFIX\/lib"),/g' settings
-cat $PREFIX/lib/ghc-8.2.2/settings
 make install
+cat $PREFIX/lib/ghc-8.2.2/settings
