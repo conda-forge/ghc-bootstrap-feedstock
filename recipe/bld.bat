01f6f2516e83c2f90e3d4772a046d858d31d10f8
Mike Sarahan <msarahan@gmail.com>
2022-08-15 21:58:54 -0500

    fix win build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
-:: SET "PATH=%PREFIX%/Library/lib:%PATH%:%BUILD_PREFIX%/Library/lib"
-bash -c "./configure --prefix=%LIBRARY_PREFIX%"
-make install
+XCOPY /s /i /d bin "%LIBRARY_BIN%"
+XCOPY /s /i /d lib "%LIBRARY_LIB%"
+XCOPY /s /i /d include "%LIBRARY_INC%"
+XCOPY /s /i /d mingw "%LIBRARY_PREFIX%\mingw"
