17e5fcd3127d6113a4b9af107205c3b004197209
mementorc <claude.rc@gmail.com>
2025-07-11 16:56:18 -0500

    (dbg) -

--- recipe/build.sh
+++ recipe/build.sh
@@ -95,8 +95,8 @@ else
   # Fix settings file for Windows
   find "${PREFIX}" -name "*mingw32*"
   find "${PREFIX}" -name "*msvcrt*"
-  find "$PREFIX" -name "libgcc*.*" -exec nm {} \; 2>/dev/null | grep chkstk
-  find "$PREFIX" -name "crt*.o" -exec nm {} \; 2>/dev/null | grep chkstk
+  find "$PREFIX" -name "libgcc*.*" -exec nm {} \; 2>/dev/null | grep chkstk || true
+  find "$PREFIX" -name "crt*.o" -exec nm {} \; 2>/dev/null | grep chkstk || true
   perl -i -pe 's#\$topdir/../mingw//bin/(llvm-)?##' "${PREFIX}"/ghc-bootstrap/lib/settings
   perl -i -pe 's#-I\$topdir/../mingw//include#-I\$topdir/../../Library/include#g' "${PREFIX}"/ghc-bootstrap/lib/settings
   perl -i -pe 's#-L\$topdir/../mingw//lib#-L\$topdir/../../Library/lib#g' "${PREFIX}"/ghc-bootstrap/lib/settings
