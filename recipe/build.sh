5ec43598e33e656ec57830e7dfe5c67d2baf23c9
mementorc <claude.rc@gmail.com>
2025-07-19 11:19:48 -0500

    (fix) osx 11. more tuning

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,10 +32,6 @@ if [[ ! -d bootstrap-ghc ]]; then
   perl -i -pe 's#($ENV{BUILD_PREFIX}|$ENV{PREFIX})/bin/##' "${settings_file}"
   perl -i -pe 's#("C compiler link flags", ")([^"]*)"#\1\2 -L\$topdir/../../../../lib"#g' "${settings_file}"
 
-  if [[ "${target_platform}" == "osx-"* ]]; then
-    perl -i -pe 's#("C compiler link flags", ")([^"]*)"#\1\2 -Wl,-rpath,@loader_path/../lib"#g' "${settings_file}"
-  fi
-
   ## RPATH to conda's lib last
   perl -i -pe 's#("C compiler link flags", ")([^"]*)"#\1\2 -Wl,-rpath,\$topdir/../../../../lib"#g' "${settings_file}"
 
@@ -94,34 +90,42 @@ else
     tar cf - ./* | (cd "${PREFIX}/ghc-bootstrap" || exit; tar xf -)
   popd 2>/dev/null || exit 1
 
+  settings_file="${PREFIX}"/ghc-bootstrap/lib/settings
+  
   # Reassign mingw references to conda Mingw
-  perl -i -pe 's#\$topdir/../mingw//bin/(llvm-)?##' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's#-I\$topdir/../mingw//include#-I\$topdir/../../Library/include#g' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's#-L\$topdir/../mingw//lib#-L\$topdir/../../Library/lib#g' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's#-L\$topdir/../mingw//x86_64-w64-mingw32/lib#-L\$topdir/../../Library/bin -L\$topdir/../../Library/x86_64-w64-mingw32/sysroot/usr/lib -Wl,-rpath,\$topdir/../../Library/x86_64-w64-mingw32/sysroot/usr/lib#g' "${PREFIX}"/ghc-bootstrap/lib/settings
+  perl -i -pe 's#\$topdir/../mingw//bin/(llvm-)?##' "${settings_file}"
+  perl -i -pe 's#-I\$topdir/../mingw//include#-I\$topdir/../../Library/include#g' "${settings_file}"
+  perl -i -pe 's#-L\$topdir/../mingw//lib#-L\$topdir/../../Library/lib#g' "${settings_file}"
+  perl -i -pe 's#-L\$topdir/../mingw//x86_64-w64-mingw32/lib#-L\$topdir/../../Library/bin -L\$topdir/../../Library/x86_64-w64-mingw32/sysroot/usr/lib -Wl,-rpath,\$topdir/../../Library/x86_64-w64-mingw32/sysroot/usr/lib#g' "${settings_file}"
 
   # Add Windows-specific compiler flags to settings
-  perl -i -pe 's/("C compiler command", ")([^"]*)"/\1x86_64-w64-mingw32-gcc.exe"/g' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's/("C\+\+ compiler command", ")([^"]*)"/\1x86_64-w64-mingw32-g++.exe"/g' "${PREFIX}"/ghc-bootstrap/lib/settings
+  perl -i -pe 's/("C compiler command", ")([^"]*)"/\1x86_64-w64-mingw32-gcc.exe"/g' "${settings_file}"
+  perl -i -pe 's/("C\+\+ compiler command", ")([^"]*)"/\1x86_64-w64-mingw32-g++.exe"/g' "${settings_file}"
+  perl -i -pe 's/(CPP command", ")([^"]*)"/\1x86_64-w64-mingw32-gcc.exe"/g' "${settings_file}"
+  perl -i -pe 's/("C compiler link flags", ")([^"]*)"/\1-fuse-ld=bfd -Wl,--enable-auto-import"/g' "${settings_file}"
 
   # Update GHC settings for Windows toolchain compatibility
-  perl -i -pe 's/("ar command", ")([^"]*)"/\1x86_64-w64-mingw32-ar.exe"/g' "${BUILD_PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's/("ar flags", ")([^"]*)"/\1qc"/g' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's/("ar supports -L", ")([^"]*)"/\1NO"/g' "${PREFIX}"/ghc-bootstrap/lib/settings
+  perl -i -pe 's/("ar command", ")([^"]*)"/\1x86_64-w64-mingw32-ar.exe"/g' "${settings_file}"
+  perl -i -pe 's/("ar flags", ")([^"]*)"/\1qc"/g' "${settings_file}"
+  perl -i -pe 's/("ar supports -L", ")([^"]*)"/\1NO"/g' "${settings_file}"
 
+  # Configure ranlib
+  perl -i -pe 's/("ranlib command", ")([^"]*)"/\1x86_64-w64-mingw32-ranlib.exe"/g' "${settings_file}"
+    
   # Force use of GNU ld instead of lld to avoid relocation type 0xe errors
-  perl -i -pe 's/("Merge objects command", ")([^"]*)"/\1x86_64-w64-mingw32-ld.exe"/g' "${BUILD_PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's/("C compiler link flags", ")([^"]*)"/\1-fuse-ld=bfd"/g' "${BUILD_PREFIX}"/ghc-bootstrap/lib/settings
+  perl -i -pe 's/("Merge objects command", ")([^"]*)"/\1x86_64-w64-mingw32-ld.exe"/g' "${settings_file}"
+  perl -i -pe 's/("Merge objects flags", ")([^"]*)"/\1-r"/g' "${settings_file}"
+  perl -i -pe 's/("Merge objects supports response files", ")([^"]*)"/\1YES"/g' "${settings_file}"
   
   # Remove clang compiler options
-  perl -i -pe 's/--rtlib=compiler-rt//g' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's/-Qunused-arguments//g' "${PREFIX}"/ghc-bootstrap/lib/settings
-  perl -i -pe 's/--target=([^ ]*)//g' "${PREFIX}"/ghc-bootstrap/lib/settings
+  perl -i -pe 's/--rtlib=compiler-rt//g' "${settings_file}"
+  perl -i -pe 's/-Qunused-arguments//g' "${settings_file}"
+  perl -i -pe 's/--target=([^ ]*)//g' "${settings_file}"
 
-  cat "${PREFIX}"/ghc-bootstrap/lib/settings
+  cat "${settings_file}"
 
   # Wrap windres
-  perl -i -pe 's#("windres command", ")[^"]*"#\1\$topdir/../bin/windres.bat"#g' "${PREFIX}"/ghc-bootstrap/lib/settings
+  perl -i -pe 's#("windres command", ")[^"]*"#\1\$topdir/../bin/windres.bat"#g' "${settings_file}"
   cp "${RECIPE_DIR}"/windres.bat "${PREFIX}"/ghc-bootstrap/bin/windres.bat
 
   # Reduce footprint
