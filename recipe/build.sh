010c8c9bf5db7ad71d731bc6a7e144f7acae0117
mementorc <claude.rc@gmail.com>
2025-09-18 21:54:58 -0500

    (fix) ncurses?

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,29 +14,29 @@ update_settings() {
     perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
     perl -i -pe "s#(C\+\+ compiler link flags\", \")([^\"]*)#\1\2 -L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
     perl -i -pe "s#(ld flags\", \")([^\"]*)#\1\2 -L\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
-    
+
   elif [[ "${target_platform}" == "linux-"* ]]; then
     settings_file="${PREFIX}"/ghc-bootstrap/lib/ghc-"${PKG_VERSION}"/lib/settings
-  
+
     # On occasion, the build_prefix was hardcoded
     perl -i -pe 's#($ENV{BUILD_PREFIX}|$ENV{PREFIX})/bin/##' "${settings_file}"
-    
+
     # Fixing the sysroot
     compiling="--sysroot=\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot"
     compiling="${compiling} -isystem=\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot/usr/include -D_GNU_SOURCE"
-    
+
     clang_linking="--sysroot=\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot"
     clang_linking="${clang_linking} -Wl,-L\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot/lib64"
     clang_linking="${clang_linking} -Wl,-L\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot/usr/lib64"
     clang_linking="${clang_linking} -Wl,-L\\\$topdir/../../../../x86_64-conda-linux-gnu/lib"
     clang_linking="${clang_linking} -Wl,-L\\\$topdir/../../../../lib"
-    
+
     ld_linking="--sysroot=\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot"
     ld_linking="${ld_linking} -L\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot/lib64"
     ld_linking="${ld_linking} -L\\\$topdir/../../../../x86_64-conda-linux-gnu/sysroot/usr/lib64"
     ld_linking="${ld_linking} -L\\\$topdir/../../../../x86_64-conda-linux-gnu/lib"
     ld_linking="${ld_linking} -L\\\$topdir/../../../../lib"
-    
+
     perl -i -pe "s#(C compiler flags\", \")#\1 ${compiling} #" "${settings_file}"
     perl -i -pe "s#(C\+\+ compiler flags\", \")#\1 ${compiling} #" "${settings_file}"
     perl -i -pe "s#(Haskell CPP flags\", \")#\1 ${compiling} #" "${settings_file}"
@@ -45,7 +45,7 @@ update_settings() {
 
   else
     settings_file="${PREFIX}"/ghc-bootstrap/lib/settings
- 
+
     # Reassign mingw references to conda-forge Mingw
     perl -i -pe 's#\$topdir/../mingw//bin/(llvm-)?##' "${settings_file}"
     perl -i -pe 's#-I\$topdir/../mingw//include#-I\$topdir/../../Library/include#g' "${settings_file}"
@@ -65,12 +65,12 @@ update_settings() {
 
     # Configure ranlib
     perl -i -pe 's/("ranlib command", ")([^"]*)"/\1x86_64-w64-mingw32-ranlib.exe"/g' "${settings_file}"
- 
+
     # Force use of GNU ld instead of lld to avoid relocation type 0xe errors
     perl -i -pe 's/("Merge objects command", ")([^"]*)"/\1x86_64-w64-mingw32-ld.exe"/g' "${settings_file}"
     perl -i -pe 's/("Merge objects flags", ")([^"]*)"/\1-r"/g' "${settings_file}"
     perl -i -pe 's/("Merge objects supports response files", ")([^"]*)"/\1YES"/g' "${settings_file}"
- 
+
     # Remove clang compiler options
     perl -i -pe 's/--rtlib=compiler-rt//g' "${settings_file}"
     perl -i -pe 's/-Qunused-arguments//g' "${settings_file}"
@@ -87,7 +87,7 @@ fine_tune_linux_rpaths() {
     "${PREFIX}"/ghc-bootstrap/bin \
     "${PREFIX}"/ghc-bootstrap/lib/ghc-"${PKG_VERSION}"/bin \
     ; do
-    
+
     if [[ "${dir}" == "${PREFIX}"/ghc-bootstrap/bin ]]; then
       rpath_str="../.."
     elif [[ "${dir}" == "${PREFIX}"/ghc-bootstrap/lib/ghc-"${PKG_VERSION}"/bin ]]; then
@@ -95,15 +95,15 @@ fine_tune_linux_rpaths() {
     else
       rpath_str="../../../../.."
     fi
-    
+
     find "${dir}" -maxdepth 1 -type f \( -executable -o -name "*.so" \) | while read -r binary; do
-      if (file "$binary" | grep -q "ELF") >& /dev/null; then
+      if (file "${binary}" | grep -q "ELF") >& /dev/null; then
         echo -n "["
-        echo -n "$(basename "$binary")"
-        
-        base_dir="$(dirname "$binary")"
- 
-        needed_libs=$(readelf -d "$binary" | grep "NEEDED" | grep -o '\[.*\]' | tr -d '[]')
+        echo -n "$(basename "${binary}")"
+
+        base_dir="$(dirname "${binary}")"
+
+        needed_libs=$(readelf -d "${binary}" | grep "NEEDED" | grep -o '\[.*\]' | tr -d '[]')
 
         need_sysroot_lib64=0
         need_sysroot_usr_lib64=0
@@ -112,38 +112,38 @@ fine_tune_linux_rpaths() {
         need_haskell_lib=0
         need_private=0
         while IFS= read -r lib; do
-          [[ -z "$lib" ]] && continue
+          [[ -z "${lib}" ]] && continue
 
           if select_path="x86_64-conda-linux-gnu/sysroot/lib64" \
-            && [[ -f "${PREFIX}/${select_path}/$lib" ]]; then
-              (cd ${base_dir} && ls $rpath_str/${select_path}/$lib | grep -q $lib) && need_sysroot_lib64=1
+            && [[ -f "${PREFIX}/${select_path}/${lib}" ]]; then
+              (cd "${base_dir}" && ls "${rpath_str}/${select_path}/${lib}" | grep -q "${lib}") && need_sysroot_lib64=1
           elif select_path="x86_64-conda-linux-gnu/sysroot/usr/lib64" \
-            && [[ -f "${PREFIX}/${select_path}/$lib" ]]; then
-              (cd ${base_dir} && ls $rpath_str/${select_path}/$lib | grep -q $lib) &&  need_sysroot_usr_lib64=1
+            && [[ -f "${PREFIX}/${select_path}/${lib}" ]]; then
+              (cd "${base_dir}" && ls "${rpath_str}/${select_path}/${lib}" | grep -q "${lib}") &&  need_sysroot_usr_lib64=1
           elif select_path="x86_64-conda-linux-gnu/lib" \
-            && [[ -f "${PREFIX}/${select_path}/$lib" ]]; then
-              (cd ${base_dir} && ls $rpath_str/${select_path}/$lib | grep -q $lib) && need_conda_lib=1
+            && [[ -f "${PREFIX}/${select_path}/${lib}" ]]; then
+              (cd "${base_dir}" && ls "${rpath_str}/${select_path}/${lib}" | grep -q "${lib}") && need_conda_lib=1
           elif select_path="ghc-bootstrap/lib/private" \
-            && [[ -f "${PREFIX}/${select_path}/$lib" ]]; then
-              (cd ${base_dir} && ls $rpath_str/${select_path}/$lib | grep -q $lib) && need_private=1
+            && [[ -f "${PREFIX}/${select_path}/${lib}" ]]; then
+               (cd "${base_dir}" && ls "${rpath_str}/${select_path}/${lib}" | grep -q "${lib}") && need_private=1
           elif select_path="ghc-bootstrap/lib/ghc-${PKG_VERSION}/lib/x86_64-linux-ghc-${PKG_VERSION}" \
-            && [[ -f "${PREFIX}/${select_path}/$lib" ]]; then
-              (cd ${base_dir} && ls $rpath_str/${select_path}/$lib | grep -q $lib) && need_haskell_lib=1
+            && [[ -f "${PREFIX}/${select_path}/${lib}" ]]; then
+              (cd "${base_dir}" && ls "${rpath_str}/${select_path}/${lib}" | grep -q "${lib}") && need_haskell_lib=1
           elif select_path="lib" \
-            && [[ -f "${PREFIX}/${select_path}/$lib" ]]; then
-              (cd ${base_dir} && ls $rpath_str/${select_path}/$lib | grep -q $lib) && need_prefix_lib=1
+            && [[ -f "${PREFIX}/${select_path}/${lib}" ]]; then
+              (cd "${base_dir}" && ls "${rpath_str}/${select_path}/${lib}" | grep -q "${lib}") && need_prefix_lib=1
           fi
-          
+
           if [[ ${need_private} == 0 ]] && [[ ${need_haskell_lib} == 0 ]] && [[ ${need_prefix_lib} == 0 ]] && [[ ${need_conda_lib} == 0 ]] && [[ ${need_sysroot_usr_lib64} == 0 ]] && [[ ${need_sysroot_lib64} == 0 ]]; then
             echo -n "DBG: NEEDED lib ${lib} - NOT FOUND"
             exit 1
           fi
         done <<< "$needed_libs"
-        
-        if (ldd "$binary" | grep -q "libgcc_s.so.1" 2>/dev/null) && ! (readelf -d "$binary" | grep -q "libgcc_s.so.1" 2>/dev/null); then
+
+        if (ldd "${binary}" | grep -q "libgcc_s.so.1" 2>/dev/null) && ! (readelf -d "${binary}" | grep -q "libgcc_s.so.1" 2>/dev/null); then
           need_conda_lib=1
         fi
-        
+
         rpath=""
         [[ $need_sysroot_lib64 == 1 ]]     && rpath="${rpath:+$rpath:}\$ORIGIN/$rpath_str/x86_64-conda-linux-gnu/sysroot/lib64"
         [[ $need_sysroot_usr_lib64 == 1 ]] && rpath="${rpath:+$rpath:}\$ORIGIN/$rpath_str/x86_64-conda-linux-gnu/sysroot/usr/lib64"
@@ -152,11 +152,11 @@ fine_tune_linux_rpaths() {
         [[ $need_haskell_lib == 1 ]]       && rpath="${rpath:+$rpath:}\$ORIGIN/$rpath_str/ghc-bootstrap/lib/ghc-${PKG_VERSION}/lib/x86_64-linux-ghc-${PKG_VERSION}"
         [[ $need_prefix_lib == 1 ]]        && rpath="${rpath:+$rpath:}\$ORIGIN/$rpath_str/lib"
 
-        current_rpath=$(patchelf --print-rpath "$binary" 2>/dev/null || echo "")
-        
+        current_rpath=$(patchelf --print-rpath "${binary}" 2>/dev/null || echo "")
+
         # Removing the RPATH damages ghc-pkg and hc-iserv-dyn-ghc
-        patchelf --force-rpath --set-rpath "${rpath}${current_rpath:+:$current_rpath}" "$binary"
-        
+        patchelf --force-rpath --set-rpath "${rpath}${current_rpath:+:$current_rpath}" "${binary}"
+
         if [[ "${binary}" != *".so"* ]]; then
           # Anchor interpreter to the versionned loader
           echo -n ":"
@@ -183,9 +183,9 @@ if [[ ! -d bootstrap-ghc ]]; then
     perl -i -pe "s|/usr/lib64/libc_nonshared.a|libc_nonshared.a|g" "$sysroot_libc_script"
     perl -i -pe "s|/lib64/ld-linux-x86-64.so.2|ld-2.17.so|g" "$sysroot_libc_script"
   fi
-  
+
   echo "Configuring ..."
-  
+
   if [[ "${target_platform}" == "linux-"* ]]; then
     bash configure \
       --prefix="${PREFIX}"/ghc-bootstrap \
@@ -203,7 +203,7 @@ if [[ ! -d bootstrap-ghc ]]; then
   fi
   echo "Running make install with sysroot environment..."
   make install >& "${SRC_DIR}"/_logs/make_install.log
-  
+
   # Update the installed settings file (find custom libraries, set sysroot, ...)
   update_settings
 
@@ -218,13 +218,13 @@ if [[ ! -d bootstrap-ghc ]]; then
     mkdir -p "${PREFIX}"/x86_64-conda-linux-gnu/{sysroot,lib}
     cp -r "${BUILD_PREFIX}"/x86_64-conda-linux-gnu/sysroot "${PREFIX}"/x86_64-conda-linux-gnu
     cp -r "${BUILD_PREFIX}"/x86_64-conda-linux-gnu/lib/libgcc_s* "${PREFIX}"/x86_64-conda-linux-gnu/lib
- 
+
     # Copy ncurses 5 shared libraries to private location
     echo "Bundling ncurses 5 libraries privately"
-    mkdir -p "${PREFIX}"/ghc-bootstrap/lib/private
-    cp "${BUILD_PREFIX}"/lib/libncurses.so.5* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
-    cp "${BUILD_PREFIX}"/lib/libtinfo.so.5* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
-    cp "${BUILD_PREFIX}"/lib/libtinfow.so.5* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
+    # mkdir -p "${PREFIX}"/ghc-bootstrap/lib/private
+    # cp "${BUILD_PREFIX}"/lib/libncurses.so.5* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
+    # cp "${BUILD_PREFIX}"/lib/libtinfo.so.5* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
+    # cp "${BUILD_PREFIX}"/lib/libtinfow.so.5* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
     
     # We seem to have an issue with using a loader different than 2.17 for ghc-pkg
     cp "${BUILD_PREFIX}"/x86_64-conda-linux-gnu/sysroot/lib64/ld-2.17.so* "${PREFIX}"/ghc-bootstrap/lib/private/ 2>/dev/null || true
@@ -243,7 +243,7 @@ if [[ ! -d bootstrap-ghc ]]; then
       echo "Signal test failed with exit code $?"
       exit 1
     fi
-  
+
     rm -rf "${PREFIX}"/x86_64-conda-linux-gnu
   fi
   
