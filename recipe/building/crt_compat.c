a404ef07a34bb00fda52d4881e6fdbe093bd38ef
mementorc <claude.rc@gmail.com>
2025-11-06 10:28:56 -0600

    (fix) crt_compat

--- recipe/building/crt_compat.c
+++ recipe/building/crt_compat.c
@@ -5,56 +5,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>
-#include <stdarg.h>
 
-/* Provide __imp__environ for legacy code */
-#if defined(_WIN64) || defined(_WIN32)
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Export _environ pointer */
-extern char ***__p__environ(void);
-
+/*
+ * Legacy __imp__environ symbol
+ * UCRT uses _get_environ() instead of direct access to _environ
+ */
 __attribute__((dllexport))
-char **__imp__environ;
+char **__imp__environ = NULL;
 
-__attribute__((constructor))
-static void init_environ(void) {
-    __imp__environ = *__p__environ();
+static void __attribute__((constructor)) init_environ(void) {
+    __imp__environ = _environ;
 }
 
-/* Provide __iob_func for legacy stdio */
+/*
+ * Legacy __iob_func for old code expecting FILE* array
+ * Modern UCRT doesn't expose __iob directly
+ */
+static FILE *iob_compat[3];
+static int iob_initialized = 0;
+
 __attribute__((dllexport))
 FILE *__iob_func(void) {
-    static FILE _iob[3] = {0};
-    static int initialized = 0;
-
-    if (!initialized) {
-        _iob[0] = *stdin;
-        _iob[1] = *stdout;
-        _iob[2] = *stderr;
-        initialized = 1;
+    if (!iob_initialized) {
+        iob_compat[0] = stdin;
+        iob_compat[1] = stdout;
+        iob_compat[2] = stderr;
+        iob_initialized = 1;
     }
-
-    return _iob;
-}
-
-/* Provide swprintf_s for legacy code that expects it */
-__attribute__((dllexport))
-int swprintf_s(wchar_t *buffer, size_t sizeOfBuffer, const wchar_t *format, ...) {
-    va_list args;
-    int ret;
-
-    va_start(args, format);
-    ret = vswprintf(buffer, sizeOfBuffer, format, args);
-    va_end(args);
-
-    return ret;
+    return iob_compat;
 }
 
 #ifdef __cplusplus
 }
-#endif
 #endif
